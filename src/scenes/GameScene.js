90204d82dd74f7c10f3c7d2d1a37e877c3c6a33d
kalavhan <josue.brigido1@gmail.com>
2020-06-01 14:00:38 -0600

    Add chronometer

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -9,16 +9,161 @@ import jungleA from '../assets/forest/bg_jungle_layers/bg5_a.png';
 import water from '../assets/forest/water.png';
 import platformTile from '../assets/forest/jungle_pack_05.png';
 import dude from '../assets/player-design/player.png';
+import bullt from '../assets/cokecan.png';
 
 export default class GameScene extends Phaser.Scene {
-  
   constructor () {
     super('Game');
+    this.gameOptions = {
+      platformSpeedRange: [300, 400],
+      spawnRange: [130, 270],
+      platformSizeRange: [90, 200],
+      platformHeightRange: [-10, 10],
+      platformHeighScale: 10,
+      platformVerticalLimit: [0.75, 0.8],
+      playerGravity: 900,
+      jumpForce: 400,
+      playerStartPosition: 200
+    }
+  }
+ 
+  preload () {
+    this.load.image('jungleG', jungleG);
+    this.load.image('jungleF', jungleF);
+    this.load.image('jungleE', jungleE);
+    this.load.image('jungleD', jungleD);
+    this.load.image('jungleC', jungleC);
+    this.load.image('jungleB', jungleB);
+    this.load.image('jungleA', jungleA);
+    this.load.image('water', water);
+    this.load.image('platform', platformTile);
+    this.load.image('bullet', bullt);
+    this.load.spritesheet("dude", dude, {
+      frameWidth: 46,
+      frameHeight: 50
+    });
+  }
+ 
+  create () {
+    this.startMusic();
+    this.setMountains();
+    this.setWater();
+    this.setPlatforms();
+    this.minutes = 0;
+    this.seconds = 0;
+    this.mls = 0;
+    this.timeText = this.add.text(100, 200, null,{fontSize: '32px', fill: '#000'});
+    this.jumpCount = 0;
+    this.player = this.physics.add.sprite(this.gameOptions.playerStartPosition, 600 * 0.6, 'dude').setDisplaySize(60, 90);
+    this.player.setGravityY(900);
+    this.anims.create({
+        key: 'idle',
+        frames: [ { key: 'dude', frame: 0 } ],
+        frameRate: 10,
+    });
+    
+    this.anims.create({
+        key: 'run',
+        frames: this.anims.generateFrameNumbers('dude', { start: 24, end: 35 }),
+        frameRate: 20,
+        repeat: -1
+    });
+
+    this.anims.create({
+      key: 'jump',
+      frames: [ { key: 'dude', frame: 6}],
+      frameRate: 10,
+      repeat: false,
+      
+    })
+    this.player.setCollideWorldBounds(true);
+    // setting collisions between the player and the platform group
+    this.physics.add.collider(this.player, this.platformGroup, null, null, this);
+
+    this.physics.add.collider(this.player, this.waterGroup, function() {
+      this.model.bgMusicPlaying = false;
+      this.bgMusic.stop();
+      this.scene.start('Title');
+    }, null, this);
+  } 
+
+  update() {
+    const cursorKey = this.input.keyboard.createCursorKeys();
+    let direction = 'right';
+    let dash = true;
+    if (cursorKey.right.isDown) {
+      this.player.flipX = direction === 'right' ? false : true;
+      if (this.player.body.touching.down) {
+        this.player.anims.play('run', true);
+        this.player.setVelocityX(400);
+      } else {
+        this.player.anims.play('jump', true);
+        this.player.setVelocityX(200);
+      }
+      direction = 'right';
+    } else if (cursorKey.left.isDown) {
+      this.player.flipX = direction === 'left' ? false : true;
+      this.player.setVelocityX(-400);
+      if (this.player.body.touching.down) {
+        this.player.anims.play('run', true);
+        this.player.setVelocityX(-400);
+      } else {
+        this.player.anims.play('jump', true);
+        this.player.setVelocityX(-350);
+      }
+      direction = 'left';
+    } else if (cursorKey.up.down) {
+      this.player.anims.play('jump', true);
+    } else {
+      this.player.setVelocityX(0);
+      this.player.anims.play('idle', true);
+    }
+    if (cursorKey.up.isDown && this.player.body.touching.down) {
+        this.player.setVelocityY(-430);
+    }
+    let minDistance = 800;
+    let rightmostPlatformHeight = 0;
+    this.platformGroup.getChildren().forEach(function(platform){
+      let platformDistance = 800 - platform.x - platform.displayWidth / 2;
+        if(platformDistance < minDistance){
+          minDistance = platformDistance;
+          rightmostPlatformHeight = platform.y;
+        }
+        if(platform.x < - platform.displayWidth / 2){
+          this.platformGroup.killAndHide(platform);
+          this.platformGroup.remove(platform);
+        }
+    }, this);
+    if(minDistance > this.nextPlatformDistance){
+      let nextPlatformWidth = Phaser.Math.Between(this.gameOptions.platformSizeRange[0], this.gameOptions.platformSizeRange[1]);
+      let platformRandomHeight = this.gameOptions.platformHeighScale * Phaser.Math.Between(this.gameOptions.platformHeightRange[0], this.gameOptions.platformHeightRange[1]);
+      let nextPlatformGap = rightmostPlatformHeight + platformRandomHeight;
+      let minPlatformHeight = 600 * this.gameOptions.platformVerticalLimit[0];
+      let maxPlatformHeight = 600 * this.gameOptions.platformVerticalLimit[1];
+      let nextPlatformHeight = Phaser.Math.Clamp(nextPlatformGap, minPlatformHeight, maxPlatformHeight);
+      this.addPlatform(nextPlatformWidth, 800 + nextPlatformWidth / 2, nextPlatformHeight);
+    }
+    this.chronometer();
   }
 
-  addSprite(x, y, asset) {
-    console.log(x);
+  chronometer() {
+    if(this.mls > 59) {
+      this.seconds += 1;
+      this.mls = 0;
+      if(this.seconds > 59){
+        this.minutes += 1;
+      }
+    } else {
+      this.mls += 1;
+    }
+    let dm = this.minutes < 10 ? `0${this.minutes}` : this.minutes;
+    let ds = this.seconds < 10 ? `0${this.seconds}` : this.seconds;
+    this.timeText.setText(`${dm}:${ds}`)
+  }
+
+  addSprite(x, y, asset, depth = 1) {
     var spriteAdded = this.physics.add.sprite(x, y, `${asset}`);
+    spriteAdded.depth = depth;
     return spriteAdded;
   }
 
@@ -38,7 +183,7 @@ export default class GameScene extends Phaser.Scene {
     let waterArray = [];
     this.waterGroup = this.add.group();
     do{
-      let water = this.addSprite(pos, 556, 'water')
+      let water = this.addSprite(pos, 586, 'water', 3)
       waterArray.push(water);
       this.waterGroup.add(water);
       i += 1;
@@ -47,100 +192,57 @@ export default class GameScene extends Phaser.Scene {
     this.tweens.add({
       targets: waterArray,
       x: '-=128',
-      duration: 2500,
+      duration: 4500,
       ease: 'Power5',
       repeat: -1
     });
   }
 
   setPlatforms() {
-    this.platformGroup = this.add.group();
-    let pos = 0;
-    let i = 0;
-    do{
-      let platform = this.addSprite(pos, 460, 'platform')
-      platform.setImmovable(true);
-      this.platformGroup.add(platform);
-      i += 1;
-      pos += 128
-    } while(i != 3)
-  }
- 
-  preload () {
-    this.load.image('jungleG', jungleG);
-    this.load.image('jungleF', jungleF);
-    this.load.image('jungleE', jungleE);
-    this.load.image('jungleD', jungleD);
-    this.load.image('jungleC', jungleC);
-    this.load.image('jungleB', jungleB);
-    this.load.image('jungleA', jungleA);
-    this.load.image('water', water);
-    this.load.image('platform', platformTile);
-    this.load.spritesheet("dude", dude, {
-      frameWidth: 46,
-      frameHeight: 50
+    this.platformGroup = this.add.group({
+      // once a platform is removed, it's added to the pool
+      removeCallback: function(platform){
+          platform.scene.platformPool.add(platform)
+      }
     });
-  }
- 
-  create () {
-    this.setMountains();
-    this.setWater();
-    this.setPlatforms();
-    this.player = this.physics.add.sprite(20, 300, 'dude').setDisplaySize(50, 70).setOrigin(0, 0);
-    this.player.setGravityY(600);
-    this.anims.create({
-        key: 'idle',
-        frames: [ { key: 'dude', frame: 0 } ],
-        frameRate: 10,
+    this.platformPool = this.add.group({
+      removeCallback: function(platform){
+          platform.scene.platformGroup.add(platform)
+      }
     });
+    this.addPlatform(800, 800 / 2, 600 * this.gameOptions.platformVerticalLimit[1]);
     
-    this.anims.create({
-        key: 'run',
-        frames: this.anims.generateFrameNumbers('dude', { start: 24, end: 35 }),
-        frameRate: 20,
-        repeat: -1
-    });
+  }
 
-    this.anims.create({
-      key: 'jump',
-      frames: this.anims.generateFrameNumbers('dude', { start: 3, end: 7 }),
-      frameRate: 10,
-      repeat: true,
-    })
-    this.player.setCollideWorldBounds(true);
-    // setting collisions between the player and the platform group
-    this.physics.add.collider(this.player, this.platformGroup, function(){
-        if(!this.player.anims.isPlaying){
-            this.player.anims.play("turn");
-        }
-    }, null, this);
-
-    this.physics.add.collider(this.player, this.waterGroup, function() {
-      this.scene.start('Title');
-    }, null, this);
-  } 
-
-  update() {
-    const cursorKey = this.input.keyboard.createCursorKeys();
-    let direction = 'right';
-    if (cursorKey.right.isDown) {
-      this.player.flipX = direction === 'right' ? false : true;
-      this.player.setVelocityX(160);
-      this.player.anims.play('run', true);
-      direction = 'right';
-    } else if (cursorKey.left.isDown) {
-      this.player.flipX = direction === 'left' ? false : true;
-      this.player.setVelocityX(-160);
-      this.player.anims.play('run', true);
-      direction = 'left';
-    } else if (cursorKey.up.down) {
-      this.player.anims.play('jump', true);
-    } else {
-      this.player.setVelocityX(0);
-      this.player.anims.play('idle', true);
+  addPlatform(platformWidth, posX, posY){
+    let platform;
+    if(this.platformPool.getLength()) {
+      platform = this.platformPool.getFirst();
+      platform.x = posX;
+      platform.active = true;
+      platform.visible = true;
+      this.platformPool.remove(platform);
     }
-    if (cursorKey.up.isDown && this.player.body.touching.down) {
-        this.player.setVelocityY(-330);
+    else {
+      platform = this.physics.add.sprite(posX, posY, "platform");
+      platform.setImmovable(true);
+      platform.setVelocityX(Phaser.Math.Between(this.gameOptions.platformSpeedRange[0], this.gameOptions.platformSpeedRange[1]) * -1);
+      this.platformGroup.add(platform);
+    }
+    platform.displayHeight = 30;
+    platform.displayWidth = platformWidth;
+    this.nextPlatformDistance = Phaser.Math.Between(this.gameOptions.spawnRange[0], this.gameOptions.spawnRange[1]);
+  }
+
+  startMusic() {
+    this.model = this.sys.game.globals.model;
+    if (this.model.musicOn === true) {
+      if(this.model.bgMusicPlaying === true){
+        this.sys.game.globals.bgMusic.stop();
+      }
+      this.bgMusic = this.sound.add('playGameMusic', { volume: 0.5, loop: true });
+      this.bgMusic.play();
+      this.sys.game.globals.bgMusic = this.bgMusic;
     }
   }
 };
--- src/scenes/Model.js
+++ src/scenes/Model.js
@@ -28,4 +28,5 @@ export default class Model {
   get bgMusicPlaying() {
     return this._bgMusicPlaying;
   }
-}
+
+};
